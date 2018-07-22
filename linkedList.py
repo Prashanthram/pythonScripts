1c771210028c4961926137f0c2f2641e5fc2aced
Prashanth Ram <prashanthram007@gmail.com>
2018-07-22 18:46:02 +0530

    Add files via upload

---
+++ linkedList.py
@@ -0,0 +1,35 @@
+﻿class node:
+    def __init__(self,data = None):
+        self.data = data
+        self.next = None
+    
+class linkedList:
+    def __init__(self):
+        self.head = node()
+    def isEmpty(self):
+        if self.head.next == None:
+            return True
+        else: return False
+    def addNode(self,data):
+        newNode = node(data)
+        curNode = self.head
+        while curNode.next != None:
+            curNode = curNode.next
+        curNode.next = newNode
+    def display(self):
+        ele =[]
+        curNode = self.head
+        while curNode.next != None:
+            curNode = curNode.next
+            ele.append(curNode.data)
+        print(ele)
+l = linkedList()
+l.addNode(6)
+l.addNode(5)
+l.addNode(4)
+l.addNode(3)
+l.addNode(2)
+l.display()
+l1 = linkedList()
+l1.addNode(5)
+print(l1.isEmpty())
---
+++ matrixMultiplication.py
@@ -0,0 +1,31 @@
+﻿def isMatrix(l):
+    n=len(l[0])
+    for i in range(1,len(l)):
+        if len(l[i])==n:
+            continue
+        else:
+            return False
+    return True
+
+def isMul(l1,l2):
+    if not isMatrix(l1) and isMatrix(l2):
+        print("Not a Matrix")
+        return False
+    if len(l1)==len(l2[0]):
+        return True
+    else:
+        return False
+
+def matmul(l1,l2):
+    if not isMul(l1,l2):
+        print("Dimension error")
+        return
+    lmul=[]
+    for i in range(len(l1)):
+        lmul.append([0 for i in range(len(l2[0]))])
+    for i in range(len(l1)):
+        for j in range(len(l2[0])):
+            for k in range(len(l2)):
+              lmul[i][j] += l1[i][k] * l2[k][j]
+    return lmul
+
